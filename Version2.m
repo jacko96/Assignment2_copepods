b582ec59c7c15581ebf37424d396509568015508
Simonrvl <161229560+Simonrvl@users.noreply.github.com>
2024-05-13 20:09:10 +0200

    Add files via upload

--- Version2.m
+++ Version2.m
@@ -5,10 +5,10 @@ close all
 %% Parameters - somewhat based on parameter sensitivity analysis
 %System parameters
 depth=100; %depth of water column [m]
-param.dz=1; %length or depth of each cell
+param.dz=1/4; %length or depth of each cell
 param.n=depth/param.dz; %number of gridcells
 param.z=0.5*param.dz:param.dz:depth-0.5*param.dz; %cell centers (FVM)
-time_duration=365*24*1; %[hours] 
+time_duration=24*365*1; %[hours] 
 
 %Light parameters
 param.I0=200; %[W m^-2] 1 W m^-2 approx. 1 μmole. m2/s ≈ 0.21739130434 W/m2.
@@ -21,18 +21,18 @@ param.gmax=0.5/24; % maximum growth rate[h^-1]
 param.alpha=0.1/24; % Light sensitivity[m^2 W^-1 h^-1]
 param.k_N=0.3*14; % nutrient half-saturation [mg N m^-3]
 param.eps=0.03/24; % natural mortality[h^-1]
-param.m=1.5/24*14; % grazing [m^3 (mg N)^-1 h^-1]
+%param.m=1.5/24*14; % grazing [m^3 (mg N)^-1 h^-1]
 param.thao=0.1/24;%0.01; % remineralization[h^-1]
 %param.Av=5*10^(-5)*(60*60); % %Vertical diffusion [m^2 h^-1]
 param.w=15/24; % sinking of detritus [m h^-1]
 param.u=0.001/24; %vertical velocity of phytoplankton [m h^-1]
-param.Nb=30*14; %bottom concentration of nutrients [mg N m^-3]
+param.Nb=30; % 30*14 -->bottom concentration of nutrients [mg N m^-3]
 
 
 %Copepods Parameters
 param.S=5; %Number of lifestages for Copepods
 
-param.CtoN=5.6;
+param.CtoN=1/5.6;
 param.v=0.0052/(24*1000)*param.CtoN; % 0.0052/(24*1000)*param.CtoN-->passive clearance rate coefficient [L mg N^{-3/4} h^{-1}]
 param.q=-1/4; %-1/4 -->passive clearance rate exponent (no exponent)
 param.h=0.4/(24*1000)*param.CtoN; % maximum ingestion rate coefficent [mg N^{1/4} h^{-1}]
@@ -43,8 +43,9 @@ param.assi=0.67; %0.67 -> assimilation rate
 param.reproc=((47+0.5)/2)/24; %0.25 -> reproduction efficiency rate
 %param.mu_ht=0.003*param.CtoN*0.001; %Mortality by higher trophic levels [mg N^{1/4} mgN^{-2} L^{-2} h^{-1}]
 param.Cs=36; %0.001 cm -->Copepod speed velocity [m/h] 
-param.b=((0.5+47)/2)/24/2; %0.37/24 -> reproduction rate [h^{-1}]
-param.mu=1/(((14+365)/2)/24); %1/(((14+365)/2)*24)/100 -> death or mortality rate of copepods [h^-1] --> based on averages found online
+%param.b=((0.5+47)/2)/24/2; %0.37/24 -> reproduction rate [h^{-1}]
+%param.mu=1/(((14+365)/2)/24); %1/(((14+365)/2)/24) -> death or mortality rate of copepods [h^-1] --> based on averages found online
+param.mu=0.003*param.CtoN/24;
 
 %Copepods grid space
 param.m0=0.01; % mgN pr individual
@@ -53,23 +54,33 @@ param.m_bound=exp(linspace(log(param.m0),log(param.ma),param.S+1)); % mgN pr ind
 param.m_center=exp(((log(param.m_bound(2:param.S+1))+log(param.m_bound(1:param.S)))/2)); % mgN pr individual
 %param.m_ratio=param.m_center(1)./param.m_center(2);
 param.m_ratio=param.m_bound(1)./param.m_bound(2);
+param.ds=log(param.m_center(2:5))-log(param.m_center(1:4));
+param.half_ds=log(param.m_center(1))-log(param.m_bound(1));
 
 
 %%  ------------------------------ Initialization and ODE-solver  ------------------------------ 
 
 % --------------------------------Initial conditions------------------------------------
-C1=1.*ones(1,param.n);
-C2=1.*ones(1,param.n);
-C3=1.*ones(1,param.n);
-C4=1.*ones(1,param.n);
-C5=1.*ones(1,param.n);
+C1=0.*ones(1,param.n);
+C2=0.*ones(1,param.n);
+C3=0.*ones(1,param.n);
+C4=0.*ones(1,param.n);
+C5=0.*ones(1,param.n);
 
-P = 0*ones(1,param.n);
-P(1:floor(param.n/4))=1000;
+C1(1:20)=5;
+C2(1:20)=5;
+C3(1:20)=5;
+C4(1:20)=5;
+C5(1:20)=5;
+
+P =0*ones(1,param.n);
+P(1:floor(param.n/4))=10;
 N = 0*ones(1,param.n);
-D = 0 * param.z; 
+N(1:5)=0.01;
+N(end-5:end)=0.01;
+D = 5.*ones(1,param.n); %To equal the abundance of copepods pr group
 
-N(1:end)=0+param.Nb/length(param.z)*param.z;
+%N(1:end)=0+param.Nb/length(param.z)*param.z;
 
 Initial=[P N D C1 C2 C3 C4 C5];
 
@@ -89,6 +100,27 @@ C5 =y(:,7*param.n+1:8*param.n);
 
 
 
+%% Light and limiting conditions
+I=calclight(t,P(end,:),D(end,:),param);
+
+sigma_N=(N(end,:)./(param.k_N+N(end,:)));
+sigma_L=(param.alpha.*I(:,end))./(sqrt(param.gmax^2+param.alpha^2.*I(:,end).^2));
+%sigma_dot=(sigma_L'.*sigma_N);
+sigma=min(sigma_L',sigma_N);
+
+figure
+plot(sigma_N,-param.z','Color','k',LineWidth=2)
+hold on
+plot(sigma_L,-param.z','Color','b',LineWidth=2)
+hold on
+plot(sigma,-param.z','r*')
+legend('Nutrient limiting','Light limiting','Limiting factor','Location','best')
+ylabel('Depth [m]')
+xlabel('Limiting factor')
+grid on; grid minor
+box on
+
+
 %%  ------------------------------ NPD Dynamics  ------------------------------ 
 
 figure('Name', "NPZD Dynamics");
@@ -102,6 +134,7 @@ colorbar;
 grid on; grid minor;
 set(gca, 'XTickLabel', []);
 set(gca, 'XTick', []);
+%clim([0 2])
 
 % Subplot for Nutrients (N)
 subplot(3, 3, 2);
@@ -132,7 +165,10 @@ title('C1');
 colorbar;
 grid on; grid minor;
 ylabel('Depth [m]');
+set(gca, 'XTickLabel', []);
+set(gca, 'XTick', []);
 %clim([1e-9 1e-4])
+ylim([-6 0])
 
 % Subplot for C2
 subplot(3, 3, 5);
@@ -141,7 +177,10 @@ shading interp;
 title('C2');
 colorbar;
 grid on; grid minor;
+set(gca, 'XTickLabel', []);
+set(gca, 'XTick', []);
 %clim([1e-9 1e-4])
+ylim([-6 0])
 
 % Subplot for C3
 subplot(3, 3, 6);
@@ -152,6 +191,7 @@ colorbar;
 grid on; grid minor;
 xlabel('Time [yr]');
 %clim([1e-9 1e-4])
+ylim([-6 0])
 
 % Subplot for C4
 subplot(3, 3, 7);
@@ -162,6 +202,7 @@ colorbar;
 grid on; grid minor;
 xlabel('Time [yr]');
 %clim([1e-9 1e-4])
+ylim([-6 0])
 
 % Subplot for C5
 subplot(3, 3, 8);
@@ -172,7 +213,7 @@ colorbar;
 grid on; grid minor;
 xlabel('Time [yr]');
 %clim([1e-9 1e-4])
-clim([0 max(max(C5))])
+ylim([-6 0])
 
 % Adjust the layout
 sgtitle('State Variables Dynamics [mgN]');
@@ -187,21 +228,22 @@ subplot(3, 3, 1);
 plot(P(end,:), -param.z','Color','b',LineWidth=2);
 title('Phytoplankton');
 grid on; grid minor;
-xlabel('Abundance [#]');
+%xlabel('Abundance [#]');
+%ylim([-10 0])
 
 % Subplot for Nutrients (N)
 subplot(3, 3, 2);
 plot(N(end,:), -param.z','Color','b',LineWidth=2);
 title('Nutrients');
 grid on; grid minor;
-xlabel('Abundance [#]');
+%xlabel('Abundance [#]');
 
 % Subplot for Detritus (D)
 subplot(3, 3, 3);
 plot(D(end,:), -param.z','Color','b',LineWidth=2);
 title('Detritus');
 grid on; grid minor;
-xlabel('Abundance [#]');
+%xlabel('Abundance [#]');
 
 % Subplot for C1
 subplot(3, 3, 4);
@@ -209,21 +251,24 @@ plot(C1(end,:), -param.z','Color','b',LineWidth=2);
 title('C1');
 grid on; grid minor;
 ylabel('Depth [m]');
-xlabel('Abundance [#]');
+%xlabel('Abundance [#]');
+ylim([-10 0])
 
 % Subplot for C2
 subplot(3, 3, 5);
 plot(C2(end,:), -param.z','Color','b',LineWidth=2);
 title('C2');
 grid on; grid minor;
-xlabel('Abundance [#]');
+%xlabel('Abundance [#]');
+ylim([-10 0])
 
 % Subplot for C3
 subplot(3, 3, 6);
 plot(C3(end,:), -param.z','Color','b',LineWidth=2);
 title('C3');
 grid on; grid minor;
-xlabel('Abundance [#]');
+xlabel('Abundance [mg N]');
+ylim([-10 0])
 
 
 % Subplot for C4
@@ -231,14 +276,16 @@ subplot(3, 3, 7);
 plot(C4(end,:), -param.z','Color','b',LineWidth=2);
 title('C4');
 grid on; grid minor;
-xlabel('Abundance [#]');
+xlabel('Abundance [mg N]');
+ylim([-10 0])
 
 % Subplot for C5
 subplot(3, 3, 8);
 plot(C5(end,:), -param.z','Color','b',LineWidth=2);
 title('C5');
 grid on; grid minor;
-xlabel('Abundance [#]');
+xlabel('Abundance [mg N]');
+ylim([-10 0])
 
 
 % Adjust the layout
@@ -246,7 +293,7 @@ xlabel('Abundance [#]');
 % linkaxes(findall(gcf, 'type', 'axes'), 'x');
 
 %% Somatic growth
-Set_depth = round(1); %Sets the depth at which you want to investigate somatic growth
+Set_depth = round(20); %Sets the depth at which you want to investigate somatic growth
 Cop_space = [C1(:,Set_depth) C2(:,Set_depth) C3(:,Set_depth) C4(:,Set_depth) C5(:,Set_depth)]; %bottom of water column
 figure()
 h = surface(t/24/365,log10(param.m_center),Cop_space');
@@ -276,9 +323,10 @@ text(0.5, param.m_center(5), 'Group 5', 'HorizontalAlignment', 'left', 'Color', 
 
 %%  ----------------------------- Grid sensitivity analysis 
 figure('Name',"Grid Sensitivity Analysis");
+param.dz=1;
 
 % Define different values of param.dz
-deltaz_values = [param.dz/4,param.dz/2,param.dz,param.dz*2,param.dz*8,param.dz*15];
+deltaz_values = [param.dz/4,param.dz/2,param.dz,param.dz*2,param.dz*4];
 
 for j = 1:length(deltaz_values)
     % Update param.dz
@@ -289,22 +337,24 @@ for j = 1:length(deltaz_values)
     param.z = 0.5 * param.dz : param.dz : depth - 0.5 * param.dz;
 
     % Initialize arrays to store results
-    C1=1.*ones(1,param.n);
-    
-    C2=1.*ones(1,param.n);
-    
-    C3=1.*ones(1,param.n);
-    
-    C4=1.*ones(1,param.n);
-    
-    C5=1.*ones(1,param.n);
+    C1=0.*ones(1,param.n);
+    C2=0.*ones(1,param.n);
+    C3=0.*ones(1,param.n);
+    C4=0.*ones(1,param.n);
+    C5=0.*ones(1,param.n);
 
-    P = 0*ones(1,param.n);
-    P(1:floor(param.n/2))=10; 
+    C1(1:20)=5;
+    C2(1:20)=5;
+    C3(1:20)=5;
+    C4(1:20)=5;
+    C5(1:20)=5;
+
+    P =0*ones(1,param.n);
+    P(1:floor(param.n/4))=10;
     N = 0*ones(1,param.n);
-    D = 0 * param.z; 
-
-    N(1:end)=0+param.Nb/length(param.z)*param.z;
+    N(1:5)=0.01;
+    N(end-5:end)=0.01;
+    D = 5.*ones(1,param.n); %To equal the abundance of copepods pr group
 
     Initial2=[P(1,:) N(1,:) D(1,:) C1(1,:) C2(1,:) C3(1,:) C4(1,:) C5(1,:)];
 
@@ -331,7 +381,7 @@ for j = 1:length(deltaz_values)
 end
 
 title('Grid Sensitivity Analysis');
-xlabel('Phytoplankton Concentration [mmol N/m^3]');
+xlabel('Copepods group 5 abundance [mg N/m^3]');
 ylabel('Depth [m]');
 legend('Location', 'best');
 grid on; grid minor;
@@ -341,17 +391,17 @@ grid on; grid minor;
 
 %% ------------------------ Parameter sensitivity analysis (and somatic growths)
 
-depth = 300; % Depth of water column [m]
-param.dz = 1; % Length or depth of each cell
+depth = 100; % Depth of water column [m]
+param.dz = 1/4; % Length or depth of each cell
 param.n = depth / param.dz; % Number of grid cells
 param.z = 0.5 * param.dz:param.dz:depth - 0.5 * param.dz; % Cell interval
 
 
-Set_depth = round(1); %Sets the depth at which you want to investigate somatic growth
+Set_depth = round(1/param.dz); %Sets the depth at which you want to investigate somatic growth
 
 
 % Define different values of specific parameter
-val= [param.Cs param.Cs/2, param.Cs/10, param.Cs*2, param.Cs*10]; %Change/update the parameter inside this vector
+val= [param.k_N*10 param.k_N*2, param.k_N, param.k_N/2, param.k_N/10]; %Change/update the parameter inside this vector
 % use either param.k_N, param.v, param.Cs, param.h, or param.eps, 
 % Define colormap
 colormap('default')
@@ -360,23 +410,28 @@ colormap('default')
 
 % Iterate over each value of specific parameter
 for i = 1:length(val)
-    % Update gmax value in param struct
-    param.Cs = val(i); %Change/update this parameter
+    % Update parameter value in param struct
+    param.k_N = val(i); %Change/update this parameter
 
     % Initialize arrays to store results
-    C1=1.*ones(1,param.n);
-    C2=1.*ones(1,param.n);
-    C3=1.*ones(1,param.n);
-    C4=1.*ones(1,param.n);
-    C5=1.*ones(1,param.n);
-    
+    C1=0.*ones(1,param.n);
+    C2=0.*ones(1,param.n);
+    C3=0.*ones(1,param.n);
+    C4=0.*ones(1,param.n);
+    C5=0.*ones(1,param.n);
 
-    P = 0*ones(1,param.n);
-    P(1:floor(param.n/2))=10;
-    N = 0*ones(1,param.n);
-    D = 0 * param.z; 
+    C1(1:20)=5;
+    C2(1:20)=5;
+    C3(1:20)=5;
+    C4(1:20)=5;
+    C5(1:20)=5;
 
-    N(1:end)=0+param.Nb/length(param.z)*param.z;
+    P =0*ones(1,param.n);
+    P(1:floor(param.n/4))=10;
+    N = 5*ones(1,param.n);
+    N(1:5)=param.Nb;
+    N(end-5:end)=param.Nb;
+    D = 5.*ones(1,param.n); %To equal the abundance of copepods pr group
 
     Initial3=[P(1,:) N(1,:) D(1,:) C1(1,:) C2(1,:) C3(1,:) C4(1,:) C5(1,:)];
 
@@ -399,15 +454,14 @@ for i = 1:length(val)
 
     % Plot P at the end of the simulation with gradient color
     figure(1)
-    plot(C5(end, :), -param.z, 'MarkerSize', 15, 'LineWidth', 2.5, 'Color', color, 'LineStyle', '-', 'DisplayName', "C5."+" = " + num2str(val(i)) + " ");
-    hold on
-    plot(P(end, :), -param.z, 'MarkerSize', 15, 'LineWidth', 2.5, 'Color', color, 'LineStyle', '--', 'DisplayName', "C1" + " = " + num2str(val(i)) + " ");
+    plot(C5(end, :), -param.z, 'MarkerSize', 15, 'LineWidth', 2.5, 'Color', color, 'LineStyle', '-', 'DisplayName', "k_{N}"+" = " + num2str(val(i)) + " mg N m^{-3} ");
+    %hold on
+    %plot(P(end, :), -param.z, 'MarkerSize', 15, 'LineWidth', 2.5, 'Color', color, 'LineStyle', '--', 'DisplayName', "C1" + " = " + num2str(val(i)) + " ");
     drawnow
     hold on;
     legend('Location', 'southeast')
     grid on; grid minor
-    title('[mmol N/m^{3}]')
-    xlabel('Copepods [mmol N/m^{3}]')
+    xlabel('# of copepods')
     ylabel('Depth [m]')
     
     figure(2)
@@ -419,7 +473,7 @@ for i = 1:length(val)
                 set(gca, 'ZScale', 'log'); % Set z-axis to logarithmic scale
                 shading interp
                 colorbar
-                title(['Somatic growth at ' num2str(Set_depth) 'm depth for ' num2str(val(i)) ]);
+                title(['Somatic growth at ' num2str(Set_depth) 'm depth for k_{N} =' num2str(val(i)) ' mg N m^{-3}']);
                 xlabel('Time [yr]')
                 ylabel('Body mass [mgN/#]')
                 % Plot ylines and add red text "group x" beside each yline
@@ -439,7 +493,7 @@ for i = 1:length(val)
                 set(gca, 'ZScale', 'log'); % Set z-axis to logarithmic scale
                 shading interp
                 colorbar
-                title(['Somatic growth at ' num2str(Set_depth) 'm depth for ' num2str(val(i)) ]);
+                title(['Somatic growth at ' num2str(Set_depth) 'm depth for k_{N} =' num2str(val(i)) ' mg N m^{-3}']);
                 xlabel('Time [yr]')
                 ylabel('Body mass [mgN/#]')
                 % Plot ylines and add red text "group x" beside each yline
@@ -459,7 +513,7 @@ for i = 1:length(val)
                 set(gca, 'ZScale', 'log'); % Set z-axis to logarithmic scale
                 shading interp
                 colorbar
-                title(['Somatic growth at ' num2str(Set_depth) 'm depth for ' num2str(val(i)) ]);
+                title(['Somatic growth at ' num2str(Set_depth) 'm depth for k_{N} =' num2str(val(i)) ' mg N m^{-3}']);
                 xlabel('Time [yr]')
                 ylabel('Body mass [mgN/#]')
                 % Plot ylines and add red text "group x" beside each yline
@@ -479,7 +533,7 @@ for i = 1:length(val)
                 set(gca, 'ZScale', 'log'); % Set z-axis to logarithmic scale
                 shading interp
                 colorbar
-                title(['Somatic growth at ' num2str(Set_depth) 'm depth for ' num2str(val(i)) ]);
+                title(['Somatic growth at ' num2str(Set_depth) 'm depth for k_{N} =' num2str(val(i)) ' mg N m^{-3}']);
                 xlabel('Time [yr]')
                 ylabel('Body mass [mgN/#]')
                 % Plot ylines and add red text "group x" beside each yline
@@ -499,7 +553,7 @@ for i = 1:length(val)
                 set(gca, 'ZScale', 'log'); % Set z-axis to logarithmic scale
                 shading interp
                 colorbar
-                title(['Somatic growth at ' num2str(Set_depth) 'm depth for ' num2str(val(i)) ]);
+                title(['Somatic growth at ' num2str(Set_depth) 'm depth for k_{N} =' num2str(val(i)) ' mg N m^{-3}']);
                 xlabel('Time [yr]')
                 ylabel('Body mass [mgN/#]')
                 % Plot ylines and add red text "group x" beside each yline
@@ -580,14 +634,14 @@ JdN(param.n+1)=-param.D*(param.Nb-N(end))/param.dz; % eller -param.D*(param.Nb-N
 
 %Detritus
 JaD(1)=0;
-JaD(param.n+1)=0; 
+JaD(param.n+1)=-param.w*D(end); 
 JdD(1)=0;
-JdD(param.n+1)=-param.w*D(end); %0 eller -param.w*D(param.n) istedet for måske
+JdD(param.n+1)=0;
 
 %Copepods
 JaC(1)=0;
 JaC(param.n+1)=0;
-JdC(1)=param.Cs*(P(2)-P(1))/(param.dz);% 0
+JdC(1)=0;%param.Cs*(P(2)-P(1))/(param.dz);% 0 ikke sikker på, om det er helt rigtigt
 JdC(param.n+1)=0;
 
 
@@ -621,7 +675,7 @@ fc = param.kappa./(param.assi*param.h).*param.m_center.^(param.p - param.h_n);
 v = param.assi*param.h.*param.m_center.^param.h_n.*(f - fc);
 g = max(0,v); % net energy gain
 mu_st = min(0,v); %mu constant?
-mu = mu_st;% + param.mu; % copepod mortality
+mu = mu_st-param.mu; % copepod mortality
 b = param.reproc.*g(:,end); % copepod birth rate
 gamma = (g - mu)./(1 - (param.m_ratio).^(1 - mu./g)); % Overgang fra lille til stor copepod
 gamma(gamma<0)=0;
@@ -640,7 +694,7 @@ dC3dt = gamma(:,2).*C2 + g(:,3).*C3 - gamma(:,3).*C3 - mu(:,3).*C3 + gradientC.*
 dC4dt = gamma(:,3).*C3 + g(:,4).*C4 - gamma(:,4).*C4 - mu(:,4).*C4 + gradientC.*(C4 > 0);
 dC5dt = gamma(:,4).*C4 - mu(:,5).*C5 + gradientC.*(C5 > 0);
 
-
+Copepod_loss=mu(:,1).*C1+mu(:,2).*C2+mu(:,3).*C3+mu(:,4).*C4+mu(:,5).*C5;
 
 
 % ----------------------------- NPD equations
@@ -650,7 +704,8 @@ Phytoplankton_loss2=Phytoplankton_loss(:,1).*C1./param.m_center(1)+Phytoplankton
 dPdt = param.gmax .* sigma .* P - param.eps .* P -Phytoplankton_loss2+ gradientP; %new equation utilizing equations from the paper
 %dPdt = param.gmax .* sigma .* P - param.eps .* P - sum(param.v.*(param.m_center.^param.q).*P/1000,2) + gradientP; % Har erstattet grazing rate med encounter rate fra ligning (1)
 dNdt = -param.gmax .* sigma .* P + param.thao .* D + gradientN; %new equation utilizing equations from the paper
-dDdt = param.eps .* P + param.m .* (P .* P) - param.thao .* D +param.thao*Phytoplankton_loss2+gradientD; %new equation utilizing equations from the paper
+dDdt = param.eps .* P  - param.thao .* D+Copepod_loss+gradientD; %new equation utilizing equations from the paper
+%dDdt = param.eps .* P  - param.thao.*D + param.m.*(P.*P) +param.thao*Phytoplankton_loss2+gradientD; %new equation utilizing equations from the paper
 
 %adding the loss of copepods to detritus equation
 
